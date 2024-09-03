32edade32644aaf49b98df52408c49075ae218ae
maria-harada <ys.training230714@gmail.com>
2024-09-03 12:54:59 +0900

    add 追加

--- src/App.tsx
+++ src/App.tsx
@@ -6,51 +6,53 @@ import { InputForm } from "./components/InputForm";
 import { List } from "./components/List";
 
 const StyledContainer = styled.div`
-  width: 327px;
-  margin: 24px auto;
+    width: 327px;
+    margin: 24px auto;
 `;
 
 export type TodoList = {
-  id: string;
-  content: string;
-  isCompleted: boolean;
-}
+    id: string;
+    content: string;
+    isCompleted: boolean;
+};
 
 function App() {
-  const [todoList, setTodoList] = React.useState<TodoList[]>([]);
+    const [todoList, setTodoList] = React.useState<TodoList[]>([]);
 
-  const getTodoList = async () => {
-    try {
-      const res = await fetch("http://localhost:3000/tasks", {
-        method: "GET",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json"
+    const getTodoList = async () => {
+        try {
+            const res = await fetch("http://localhost:3000/tasks", {
+                method: "GET",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (!res.ok) {
+                throw new Error();
+            }
+
+            const data = await res.json();
+            setTodoList(data);
+        } catch (error) {
+            alert("「npm run json-server」でjson-serverを起動してください。");
         }
-      })
+    };
 
-      if(!res.ok) {
-        throw new Error();
-      }
+    React.useEffect(() => {
+        getTodoList();
+    }, []);
 
-      const data = await res.json();
-      setTodoList(data);
-    } catch (error) {
-      alert('「npm run json-server」でjson-serverを起動してください。')
-    }
-  }
-
-  React.useEffect(() => {
-    getTodoList();
-  }, []);
-
-  return (
-    <StyledContainer>
-      <Typography variant="h1" sx={{fontSize: "32px"}}>今日やること</Typography>
-      <InputForm getData={getTodoList} />
-      <List getData={getTodoList} todoList={todoList} />
-    </StyledContainer>
-  )
+    return (
+        <StyledContainer>
+            <Typography variant="h1" sx={{ fontSize: "32px" }}>
+                今日やること
+            </Typography>
+            <InputForm getData={getTodoList} />
+            {/* <List getData={getTodoList} todoList={todoList} /> */}
+        </StyledContainer>
+    );
 }
 
 export default App;
